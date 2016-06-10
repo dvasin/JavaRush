915510d7d7052ba54564f0dd729b29b63a15729b
Denis <st1xx@mail.ru>
2016-06-10 21:03:33 +0300

    6.06

---
+++ src/com/javarush/test/Test.java
@@ -0,0 +1,54 @@
+package com.javarush.test;
+
+/**
+ * Created by denis on 09.06.16.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        int height = 3;
+        int width = 4;
+
+
+        int[][] matrix = new int[height][width];
+        matrix[2][0] = 1;
+        matrix[2][1] = 1;
+        matrix[2][2] = 1;
+        matrix[2][3] = 1;
+
+        matrix[1][0] = 1;
+        matrix[1][1] = 1;
+        matrix[1][2] = 1;
+        matrix[1][3] = 1;
+
+        matrix[0][0] = 1;
+        matrix[0][1] = 1;
+        matrix[0][2] = 1;
+
+        for (int i = 0; i < height; i++) {
+            // Цикл по второй размерности выводит колонки - вывод одной строки
+            for (int j = 0; j < width; j++) {
+                // Используем оператор print - без перехода на следующую строку
+                System.out.print(matrix[i][j]);
+            }
+            // Переход на следующую строку
+            System.out.println();
+        }
+
+
+        System.out.println();
+
+        for (int i = 0; i < height; i++) {
+            // Цикл по второй размерности выводит колонки - вывод одной строки
+            for (int j = 0; j < width; j++) {
+                // Используем оператор print - без перехода на следующую строку
+                System.out.print(matrix[i][j]);
+            }
+            // Переход на следующую строку
+            System.out.println();
+        }
+
+
+    }
+}
+
